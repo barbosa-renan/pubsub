6c5b5a20c763331f0e02a78df146134f8106b7f1
Renan Barbosa <renanvinicius.quadros@gmail.com>
2023-10-17 21:04:37 -0300

    first commit

---
+++ GenericPublisher/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+
+namespace GenericPublisher
+{
+    public static class Extensions
+    {
+        public static void ResilientSend(this IModel model, string exchangeName, string routingKey, IBasicProperties prop, object objectToSend)
+        {
+            if (prop == null)
+            {
+                prop = model.CreateBasicProperties();
+            }
+            prop.DeliveryMode = 2;
+
+            var serialized = System.Text.Json.JsonSerializer.Serialize(objectToSend);
+
+            var bytesToSend = System.Text.Encoding.UTF8.GetBytes(serialized);
+
+            if (string.IsNullOrEmpty(routingKey))
+                routingKey = string.Empty;
+
+            model.BasicPublish(exchangeName, routingKey, prop, bytesToSend);
+        }
+    }
+}
---
+++ GenericPublisher/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using GenericPublisher;
+using RabbitMQ.Client;
+
+ConnectionFactory connectionFactory = new ConnectionFactory()
+{
+    UserName = "adm",
+    Password = "123",
+    Port = 5672,
+    HostName = Environment.GetEnvironmentVariable("RABBITMQ_HOST"),
+    VirtualHost = "main"
+};
+
+using var connection = connectionFactory.CreateConnection();
+using var model = connection.CreateModel();
+
+string exchangeName = Environment.GetEnvironmentVariable("RABBITMQ_EXCHANGE");
+string routingKey = Environment.GetEnvironmentVariable("RABBITMQ_ROUNTINGKEY");
+int max = int.Parse(Environment.GetEnvironmentVariable("RABBITMQ_MAX"));
+
+
+for (int i = 0; i <= max; i++)
+{
+    var prop = model.CreateBasicProperties();
+    prop.DeliveryMode = 2;
+    prop.ContentType = "application/json";
+    prop.Headers = new Dictionary<string, object>()
+    {
+        {"DATA", DateTime.UtcNow.ToString("U") }
+    };
+
+    model.ResilientSend(exchangeName, routingKey, prop, new { value = i, OS = Environment.OSVersion.ToString() });
+}
+
+Console.WriteLine("Awesome!");
